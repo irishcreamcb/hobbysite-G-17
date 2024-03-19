3e96ba8430f388b999e1ff9048af77128bd19167
ADMU-Beaux <beaux.consunji@student.ateneo.edu>
2024-03-20 01:11:40 +0800

    Create views

--- wiki/views.py
+++ wiki/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from typing import Any
+from django.db.models.query import QuerySet
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 
-# Create your views here.
+from .models import *
+
+class ArticleListView(ListView):
+    model = Article
+    template_name = 'wiki/list.html'
+
+class ArticleDetailView(DetailView):
+    model = Article
+    template_name = 'wiki/detail.html'
