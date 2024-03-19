69a72b4a5f906c2b5936f05d11f5a769b122e9df
ADMU-Beaux <beaux.consunji@student.ateneo.edu>
2024-03-20 01:10:35 +0800

    Add to admin panel

--- wiki/admin.py
+++ wiki/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import *
+
+class ArticleCategoryAdmin(admin.ModelAdmin):
+    model = ArticleCategory
+
+class ArticleAdmin(admin.ModelAdmin):
+    model = Article
+
+admin.site.register(ArticleCategory, ArticleCategoryAdmin)
+admin.site.register(Article, ArticleAdmin)
