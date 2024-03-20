b5fab37f1039497130ac2fca8fa88cf751274c1c
sadelrosario <sherrie.delrosario@student.ateneo.edu>
2024-03-20 20:46:10 +0800

    added features

--- merchstore/models.py
+++ merchstore/models.py
@@ -1,3 +1,35 @@
 from django.db import models
+from django.urls import reverse
+
 
 # Create your models here.
+class ProductType(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    
+    def __str__(self):
+        return self.name
+    
+    class Meta: 
+        ordering = ['name']
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    Product_Type = models.ForeignKey(
+        'ProductType', 
+        on_delete=models.SET_NULL,
+        null=True,
+        related_name='type'
+    )
+    description = models.TextField()
+    price = models.DecimalField(max_digits=7, decimal_places=2)
+    
+    def __str__(self):
+        return self.name
+    
+    def get_absolute_url(self):
+        return reverse('merchstore:item-detail', args=[self.pk])
+
+    class Meta: 
+        ordering = ['name']
--- merchstore/views.py
+++ merchstore/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.views.generic.detail import DetailView
+from django.views.generic.list import ListView
+from .models import Product, ProductType
 
 # Create your views here.
+class ItemList(ListView):
+    model = Product
+    template_name = "items_list.html"
+
+class ItemDetail(DetailView):
+    model = Product
+    template_name = "item_info.html"
