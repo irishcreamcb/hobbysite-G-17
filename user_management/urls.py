9063b28f1d31e50fe22012b4fd48f980206ed8fc
irishcreamcb <157715369+irishcreamcb@users.noreply.github.com>
2024-05-01 22:39:05 +0800

    Changed path of update view in user_management to /profile instead of /update.

--- user_management/urls.py
+++ user_management/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from .views import ProfileUpdateView
 
 urlpatterns = [ 
-    path('/update', ProfileUpdateView.as_view(), name = 'profile-update')
+    path('/profile', ProfileUpdateView.as_view(), name = 'profile-update')
 ]
 
 app_name = 'user_management'
