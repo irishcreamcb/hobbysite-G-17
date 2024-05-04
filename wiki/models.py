d0e572cbac856739cf46aae9775d93134fe924f8
BeauxConsunji <beauxconsunji@gmail.com>
2024-05-04 20:06:09 +0800

    Update models

--- wiki/models.py
+++ wiki/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.urls import reverse
+from user_management.models import *
 
 class ArticleCategory(models.Model):
     name = models.CharField(max_length=255)
@@ -15,8 +16,10 @@ class ArticleCategory(models.Model):
 
 class Article(models.Model):
     title = models.CharField(max_length=255)
+    author = models.ForeignKey(Profile, on_delete=models.SET_NULL)
     category = models.ForeignKey(ArticleCategory, null=True, on_delete=models.SET_NULL)
     entry = models.TextField()
+    header_image = models.ImageField(upload_to="images/")
     created_on = models.DateTimeField(auto_now_add=True)
     updated_on = models.DateTimeField(auto_now=True)
 
@@ -25,6 +28,16 @@ class Article(models.Model):
     
     def get_absolute_url(self):
         return reverse('wiki:detail', args=[str(self.pk)])
+
+    class Meta:
+        ordering = ["-created_on"]
+
+class Comment(models.Model):
+    author = models.ForeignKey(Profile, on_delete=models.SET_NULL)
+    article = models.ForeignKey(Article, on_delete=models.CASCADE)
+    entry = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
 
     class Meta:
         ordering = ["-created_on"]
