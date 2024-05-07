3fed904a737bf59824707b2509c156cf55deb3db
BeauxConsunji <beauxconsunji@gmail.com>
2024-05-08 03:53:18 +0800

    Set up media files

--- hobbysite/settings.py
+++ hobbysite/settings.py
@@ -131,6 +131,8 @@ STATICFILES_DIRS = [
 STATIC_URL = '/static/'
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/user_management/login'
+MEDIA_ROOT = BASE_DIR/'media'
+MEDIA_URL = '/media/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
