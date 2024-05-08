13eb587fdd2ed850157ca8d34690f2259b880e7a
BeauxConsunji <beauxconsunji@gmail.com>
2024-05-08 22:17:07 +0800

    Fix profile update success

--- user_management/models.py
+++ user_management/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.urls import reverse
 
 
 class Profile(models.Model): 
@@ -9,3 +10,6 @@ class Profile(models.Model):
 
     def __str__(self): 
         return self.display_name
+
+    def get_absolute_url(self):
+        return reverse('user_management:profile-update', args=[str(self.pk)])
