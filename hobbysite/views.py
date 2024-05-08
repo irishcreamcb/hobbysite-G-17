24cd6145c137cf10e29469529e616ea692f5a45b
BeauxConsunji <beauxconsunji@gmail.com>
2024-05-09 00:21:22 +0800

    Make migrations

--- hobbysite/views.py
+++ hobbysite/views.py
@@ -1,8 +1,7 @@
 from django.shortcuts import render
 from django.views import View
 from user_management.models import Profile
-from commissions.models import Commission, Job, JobApplication
-from merchstore.models import Product, Transaction
+from commissions.models import Commission
 from blog.models import Article as BlogArticle
 from wiki.models import Article as WikiArticle
 
@@ -13,10 +12,6 @@ class IndexView(View):
             profile = Profile.objects.get(user=self.request.user)
             ctx["blog_articles"] = BlogArticle.objects.filter(author=profile)
             ctx["wiki_articles"] = WikiArticle.objects.filter(author=profile)
-            ctx["commissions_created"] = Commission.objects.filter(creator=profile)
-            ctx["commissions_joined"] = list(set([job.commission for job in [job_application.job for job_application in JobApplication.objects.filter(applicant=profile)]]))
-            ctx["products_bought"] = list(set([transaction.product for transaction in Transaction.objects.filter(buyer=profile)]))
-            ctx["products_sold"] = Product.objects.filter(owner=profile)
-            
+            ctx["commissions"] = Commission.objects.filter(creator=profile)
         return render(request, "index.html", ctx)
 
